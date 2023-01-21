27f412c73d0d2d96840750cbe2b3166f9fa6fccf
alahirajeffrey <alahirajeffrey@gmail.com>
2023-01-21 20:55:25 +0100

    updated defaut route

--- src/server.ts
+++ src/server.ts
@@ -5,7 +5,7 @@ dotenv.config();
 
 const server: Express = express();
 
-server.get("/", (req: Request, res: Response) => {
+server.get("/api/v1/", (req: Request, res: Response) => {
   return res.status(200).json({ message: "welcome to the homepage" });
 });
 
