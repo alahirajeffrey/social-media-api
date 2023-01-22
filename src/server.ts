4578661694fef435b6cdf15990986941e7795768
alahirajeffrey <alahirajeffrey@gmail.com>
2023-01-23 00:07:21 +0100

    server setup

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,13 @@
 import express, { Express, Request, Response } from "express";
 import dotenv from "dotenv";
+import authRouter from "./auth/auth.router";
 
 dotenv.config();
 
 const server: Express = express();
+
+server.use(express.json());
+server.use("/api/v1/", authRouter);
 
 server.get("/api/v1/", (req: Request, res: Response) => {
   return res.status(200).json({ message: "welcome to the homepage" });
