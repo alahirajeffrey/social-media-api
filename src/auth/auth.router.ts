059954990103202b0218a19287d398f8bcc669cd
alahirajeffrey <alahirajeffrey@gmail.com>
2023-01-26 00:34:29 +0100

    merged changes in main

--- src/auth/auth.router.ts
+++ src/auth/auth.router.ts
@@ -1,8 +1,5 @@
 import { Router } from "express";
-<<<<<<< HEAD
-=======
 import { verifyToken } from "../utils/verifyToken";
->>>>>>> d1969681e71731d8915b198a1d281346505b5799
 import * as auth from "./auth.controller";
 
 const authRouter = Router();
@@ -18,10 +15,7 @@ authRouter.patch("/forgot-password", auth.forgotPassword);
 authRouter.post("/send-verification-mail", auth.sendVerificationMail);
 
 authRouter.post("/verify-email", auth.verifyEmail);
-<<<<<<< HEAD
-=======
 
 authRouter.post("/logout", auth.logoutUser);
->>>>>>> d1969681e71731d8915b198a1d281346505b5799
 
 export default authRouter;
--- src/server.ts
+++ src/server.ts
@@ -6,10 +6,7 @@ const server: Express = express();
 
 server.use(express.json());
 server.use("/api/v1/auth", authRouter);
-<<<<<<< HEAD
 server.use("/api/v1/profile", profileRouter);
-=======
->>>>>>> d1969681e71731d8915b198a1d281346505b5799
 
 server.get("/api/v1/", (req: Request, res: Response) => {
   return res.status(200).json({ message: "welcome to the homepage" });
