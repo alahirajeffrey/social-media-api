61a7d0f2a31305a607caa5f7574b6d3f59a161c3
alahirajeffrey <alahirajeffrey@gmail.com>
2023-01-26 00:17:08 +0100

    protected routes

--- src/auth/auth.router.ts
+++ src/auth/auth.router.ts
@@ -1,4 +1,5 @@
 import { Router } from "express";
+import { verifyToken } from "../utils/verifyToken";
 import {
   changePassword,
   forgotPassword,
@@ -14,12 +15,16 @@ authRouter.post("/auth/register", registerUser);
 
 authRouter.post("/auth/login", loginUser);
 
-authRouter.patch("/auth/change-password", changePassword);
+authRouter.patch("/auth/change-password", verifyToken, changePassword);
 
 authRouter.patch("/auth/forgot-password", forgotPassword);
 
-authRouter.post("/auth/send-verification-mail", sendVerificationMail);
+authRouter.post(
+  "/auth/send-verification-mail",
+  verifyToken,
+  sendVerificationMail
+);
 
-authRouter.post("/auth/verify-email", verifyEmail);
+authRouter.post("/auth/verify-email", verifyToken, verifyEmail);
 
 export default authRouter;
