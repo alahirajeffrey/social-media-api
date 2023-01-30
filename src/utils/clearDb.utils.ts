fae8e3ee04587c8e013794e67cf038008bf34d34
alahirajeffrey <alahirajeffrey@gmail.com>
2023-01-30 22:19:47 +0100

    code refactor

--- src/utils/clearDb.utils.ts
+++ src/utils/clearDb.utils.ts
@@ -1,18 +1,11 @@
 import { PrismaClient } from "@prisma/client";
 const prisma = new PrismaClient();
 
-export const clearUserTable = async () => {
-  return await prisma.user.deleteMany({ where: {} });
-};
-
-export const clearProfileTable = async () => {
-  return await prisma.profile.deleteMany({ where: {} });
-};
-
-export const clearCommentTable = async () => {
-  return await prisma.comment.deleteMany({ where: {} });
-};
-
-export const clearPostTable = async () => {
-  return await prisma.post.deleteMany({ where: {} });
+export const clearTables = async () => {
+  return Promise.all([
+    prisma.comment.deleteMany({ where: {} }),
+    prisma.post.deleteMany({ where: {} }),
+    prisma.profile.deleteMany({ where: {} }),
+    prisma.user.deleteMany({ where: {} }),
+  ]);
 };
