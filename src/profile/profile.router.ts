10b3162b7891ae720b4f88ef4fb28c271685f306
alahirajeffrey <alahirajeffrey@gmail.com>
2023-01-30 22:19:14 +0100

    added profile routes

--- src/profile/profile.router.ts
+++ src/profile/profile.router.ts
@@ -1,21 +1,31 @@
 import { Router } from "express";
 import * as profile from "./profile.controller";
+import { verifyToken } from "../utils/verifyToken";
 
 const profileRouter = Router();
 
-profileRouter.post("/create-profile", profile.createProfile);
+profileRouter.post("/create-profile", verifyToken, profile.createProfile);
 
-profileRouter.get("/:profileId", profile.getProfileById);
+profileRouter.get("/:email", profile.getProfileByEmail);
 
-profileRouter.patch("/follow/:profileId/:followerId", profile.followProfile);
+profileRouter.patch(
+  "/follow/:profileId/:followerId",
+  verifyToken,
+  profile.followProfile
+);
 
 profileRouter.patch(
   "/unfollow/:profileId/:followingId",
+  verifyToken,
   profile.unfollowProfile
 );
 
-profileRouter.get("/followers/:profileId", profile.getFollowers);
+// profileRouter.get("/followers/:profileId", verifyToken, profile.getFollowers);
 
-profileRouter.get("/following/:profileId", profile.getPeopleFollwed);
+// profileRouter.get(
+//   "/following/:profileId",
+//   verifyToken,
+//   profile.getPeopleFollowed
+// );
 
 export default profileRouter;
